d2d8e3eb651e273bd667e3030617fb4eab990e2f
Brayan Gonzalez <brayan.a.gonzalez@gmail.com>
2025-05-08 23:44:32 -0400

    add zsh install

--- install.sh
+++ install.sh
@@ -1,25 +1,16 @@
 #!/usr/bin/env bash
 
 # Install dependencies
-sudo apt update
-sudo apt upgrade -y
-sudo apt install kitty zsh curl git build-essential cmake vlc stow -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install neovim
-git clone https://github.com/neovim/neovim Code/nvim
-cd Code/nvim
-git checkout stable
-ggpull
-make CMAKE_BUILD_TYPE=RelWithDebInfo
-sudo make install
-cd $HOME
+sudo pacman -Syyu kitty curl git cmake vlc stow nvim adobe-source-han-sans-jp-fonts
 
 # Install zellij
 
 
-chmod +x Code/scripts/install_zellij.sh
-./Code/scripts/install_zellij.sh
+chmod +x install_zellij.sh
+./install_zellij.sh
 
-cd $HOME/Code/scripts/dotfiles
+chmod +x install_zsh.sh
+./install_zsh.sh
+
+cd $HOME/Code/install/dotfiles
 stow -v --adopt --target=$HOME/.config .config
---
+++ install_zsh.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo pacman -S zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
