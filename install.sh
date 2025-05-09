dc237a926f48e49ffc1c024f9f4b0b108c305d19
Brayan Gonzalez <brayan.a.gonzalez@gmail.com>
2025-05-09 00:05:34 -0400

    add rbenv and docker

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install dependencies
-sudo pacman -Syyu kitty curl git cmake vlc stow nvim adobe-source-han-sans-jp-fonts
+sudo pacman -Syyu kitty curl git cmake vlc stow nvim adobe-source-han-sans-jp-fonts rbenv docker
 
 # Install zellij
 
@@ -11,6 +11,9 @@ chmod +x install_zellij.sh
 
 chmod +x install_zsh.sh
 ./install_zsh.sh
+
+chmod +x install_rbenv.sh
+./install_rbenv.sh
 
 cd $HOME/Code/install/dotfiles
 stow -v --adopt --target=$HOME/.config .config
