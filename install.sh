b3d3efac54ed350336fe7ee51067de175114fecb
Brayan <brayan.a.gonzalez@gmail.com>
2025-07-15 14:23:40 -0400

    no more swayfx

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Install dependencies
-sudo pacman -Syu --noconfirm kitty curl git cmake base-devel vlc stow nvim adobe-source-han-sans-jp-fonts rbenv nvm waybar otf-font-awesome xdg-desktop-portal-wlr dolphin networkmanager obsidian
+sudo pacman -Syu --noconfirm sway kitty curl git cmake base-devel vlc stow nvim adobe-source-han-sans-jp-fonts rbenv nvm waybar otf-font-awesome xdg-desktop-portal-wlr dolphin networkmanager obsidian
 
 # Install yay
 chmod +x install_yay.sh
 ./install_yay.sh
 
-yay -S flameshot-gui brave-git swayfx zellij
+yay -Syu flameshot-git brave-bin zellij
 
 chmod +x install_zsh.sh
 ./install_zsh.sh
