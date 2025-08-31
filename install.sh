d8dd1bb2d7f9052fc9ab0ff82211a1ae31598977
Brayan Gonzalez <brayan.a.gonzalez@gmail.com>
2025-08-31 12:31:16 -0400

    arch hyprland

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Install dependencies
-sudo pacman -Syu --noconfirm sway kitty curl git cmake base-devel vlc stow nvim adobe-source-han-sans-jp-fonts rbenv nvm waybar otf-font-awesome xdg-desktop-portal-wlr dolphin networkmanager obsidian
+sudo pacman -Syu --noconfirm hyprpaper kitty curl git cmake base-devel vlc stow nvim adobe-source-han-sans-jp-fonts rbenv nvm waybar otf-font-awesome networkmanager man pipewire-pulse
 
 # Install yay
 chmod +x install_yay.sh
 ./install_yay.sh
 
-yay -Syu flameshot-git brave-bin zellij
+yay -Syu flameshot-git zellij
 
 chmod +x install_zsh.sh
 ./install_zsh.sh
