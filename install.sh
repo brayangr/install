5419dade4110cd32f399ede5fc087d585d250903
Brayan Gonzalez <brayan.a.gonzalez@gmail.com>
2025-09-24 21:21:27 -0300

    config swayosd

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 # Install dependencies
-sudo pacman -Syu --noconfirm hyprpaper kitty curl git cmake base-devel vlc stow nvim adobe-source-han-sans-jp-fonts rbenv nvm waybar otf-font-awesome networkmanager man pipewire-pulse discord ttf-jetbrains-mono-nerd
+sudo pacman -Syu --noconfirm hyprpaper kitty curl git cmake base-devel vlc stow nvim adobe-source-han-sans-jp-fonts rbenv nvm waybar otf-font-awesome networkmanager man pipewire-pulse discord ttf-jetbrains-mono-nerd alsa-scarlett-gui
 
 # Install yay
 chmod +x install_yay.sh
 ./install_yay.sh
 
-yay -Syu flameshot-git zellij spotify visual-studio-code-bin
+yay -Syu flameshot-git zellij spotify visual-studio-code-bin swayosd
+
+# Enable Swayosd service
+sudo systemctl enable --now swayosd-libinput-backend.service
 
 chmod +x install_zsh.sh
 ./install_zsh.sh
